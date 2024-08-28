196ff7e4378f003701ace540ae7d7b39e5028c86
William-Oestmann <willoestmann04@gmail.com>
2024-08-28 16:40:31 +0000

    First update

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,14 +6,17 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print ("hello")
   #Ask for the user's name
-
+  name = input("What is your name? ")
+  print("Good to meet you", name)
   #Use the user's name in the program.
-
+  
   #Ask the user for their age.
-
+  age = input("How old are you? ")
+  print("You were born in", 2023 - int(age))
   #Tell the user what year they were born in.
+  
   #Assume that they have not had their birthday yet this year.
 
 
--- MadLib.py
+++ MadLib.py
@@ -6,9 +6,14 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  sport = input("Enter a sport: ")
+  name = input("Enter a name: ")
+  adjective = input("Enter a adjective: ")
+  place = input("Enter a place: ")
+  number = input("Enter a number: ")
+  animal = input("Enter an animal: ")
   #Print the story with the user supplied words.
-
+  print("I love playing",sport, "with my best friend",name + ".","My best friend is very",adjective,"because they love going to the",place + ". During",sport,"I can jump",number,"feet high! A little off topic, but I wish",animal + "s did not exist!")
 
 
 #Call the main function if this is the file being run.
